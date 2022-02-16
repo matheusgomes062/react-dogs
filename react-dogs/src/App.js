0ae8bfb82d9403e7976bd14732e4e6c2d4847520
Matheus Gomes <matheusgomes062@gmail.com>
2022-02-16 20:36:54 -0300

    feat: major changes

--- react-dogs/src/App.js
+++ react-dogs/src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Components/Home";
+import Login from "./Components/Login/Login/Login";
+import { UserStorage } from "./UserContext";
+import User from "./Components/User/User";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
+import Photo from "./Components/Photo/Photo";
+import UserProfile from "./Components/User/UserProfile";
+import NotFound from "./Components/NotFound";
 
 function App() {
   return (
     <div className="App">
-      App React
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <main className="AppBody">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
+            <Route path="foto/:id" element={<Photo />} />
+            <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          </main>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
     </div>
   );
 }
